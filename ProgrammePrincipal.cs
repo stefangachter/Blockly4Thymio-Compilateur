6fc4cc8e6d5c7243659f8fa7ac6cca7e69403b26
Okimi- <contact@okimi.net>
2016-01-05 16:57:29 +0100

    Corrections sur les noms des fichiers

---
+++ ProgrammePrincipal.cs
@@ -0,0 +1,34 @@
+﻿
+using	System;
+using	System.IO;
+using	System.Reflection;
+using	System.Windows.Forms;
+
+
+
+namespace 		Compilateur {
+static	class 	ProgrammePrincipal {
+
+	/// <summary>
+	/// Point d'entrée principal de l'application.
+	/// </summary>
+	[STAThread]
+	static void Main() {
+
+		// A compléter...
+
+
+
+		// Traitements
+		// -----------
+
+		Application.EnableVisualStyles();
+		Application.SetCompatibleTextRenderingDefault( false );
+		Application.Run( new FEN_Principale() );
+
+	}
+
+}
+}
+
+
