3c17055feccf5ba88e893837b81d6503f9427416
Okimi <contact@okimi.net>
2017-03-12 15:30:41 +0100

    Préparation du paquet .deb

---
+++ package-debian/blockly4thymio/usr/bin/blockly4thymio.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mono --runtime=v4.0 ./Compilateur.1.2.exe $1
---
+++ package-debian/blockly4thymio/usr/lib/blockly4thymio/interface-hors-ligne/blockly4thymio/load.js
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview Load and save functions
+ * @author emmanuel.fort@gmail.com
+ */
+'use strict';
+
+
+
+function  handleSaveButtonClick() {
+  var xml = Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace());  
+  xml = Blockly.Xml.domToText(xml);
+  xml = encodeURI(xml);
+  xml = xml.replace( /#/g, "%23" );
+  var link = document.getElementById('saveLink');
+  link.download = "programme.b4t";
+  link.href = "data:application/octet-stream," + xml;
+  link.click();  
+}
+
+
+function handleLoadButtonClick(evt) {
+	var files = evt.target.files; // FileList object
+	var file = files[0];
+	var fileReader = new FileReader();
+	fileReader.onload = (function(theFile) {
+      return function(e) {
+        //alert(fileReader.result);
+		var xml = Blockly.Xml.textToDom(fileReader.result);
+		Blockly.Xml.domToWorkspace(Blockly.getMainWorkspace(), xml);
+      };
+    })(file);
+	fileReader.readAsText(file);
+}
+
+
+    
